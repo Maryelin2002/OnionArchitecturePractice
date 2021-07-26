b1a8d884d1708a5c399072331029f444356eb753
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-07-26 10:56:14 -0400

    AthenticateRequestDto And AuthenticateResponseDto Added

---
+++ SolvexWorkShop.Bl/Dto/AuthenticateResponseDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolvexWorkShop.Bl.Dto
+{
+    public class AuthenticateResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponseDto(UserDto user, string token)
+        {
+            Id = user.Id.GetValueOrDefault();
+            Name = user.Name;
+            LastName = user.LastName;
+            UserName = user.UserName;
+            Token = token;
+        }
+    }
+}
