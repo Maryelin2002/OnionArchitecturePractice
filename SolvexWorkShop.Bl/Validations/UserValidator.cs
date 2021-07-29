c07cb0fe708fd76d732e16a47ac540abd5b93c28
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-07-29 12:37:07 -0400

    Modified UserValidator Rules

--- SolvexWorkShop.Bl/Validations/UserValidator.cs
+++ SolvexWorkShop.Bl/Validations/UserValidator.cs
@@ -9,16 +9,16 @@ namespace SolvexWorkShop.Bl.Validations
         {
             RuleFor(x => x.Name)
                 .NotEmpty()
-                .WithMessage("Member's Name is required");
+                .WithMessage("User's Name is required");
             RuleFor(x => x.LastName)
                 .NotEmpty()
-                .WithMessage("Member's LastName is required");
+                .WithMessage("User's LastName is required");
             RuleFor(x => x.DocumentType)
-                .NotEmpty()
-                .WithMessage("Member's DocumentType is required");
+                .NotNull()
+                .WithMessage("User's DocumentType is required");
             RuleFor(x => x.Gender)
-                .NotEmpty()
-                .WithMessage("Member's Gender is required");
+                .NotNull()
+                .WithMessage("User's Gender is required");
         }
     }
 }
