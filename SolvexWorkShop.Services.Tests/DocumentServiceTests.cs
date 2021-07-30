21cba59c53ebb282cfb8cb90ad9d345bcebb71cd
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-07-29 21:45:50 -0400

    Services Tests Complete

---
+++ SolvexWorkShop.Services.Tests/DocumentServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using SolvexWorkshop.Model.Repositories;
+using SolvexWorkShop.Bl.Dto;
+using SolvexWorkShop.Bl.Validations;
+using SolvexWorkShop.Services.Services;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SolvexWorkShop.Services.Tests
+{
+    public class DocumentServiceTests : IClassFixture<ServicesTestsFixture>
+    {
+        private readonly IDocumentService _documentService;
+
+        public DocumentServiceTests(ServicesTestsFixture fixture)
+        {
+            IDocumentRepository repository = new DocumentRepository(fixture.Context);
+
+            var validator = new DocumentValidator();
+
+            _documentService = new DocumentService(repository, fixture.Mapper, validator);
+        }
+
+        [Fact]
+        public async Task ShouldSaveDocumentAsync()
+        {
+            //Arrange
+            var requestDto = new DocumentDto
+            {
+                FileName = "Prueba",
+                OriginalName = "Prueba",
+                ContentType = "Adobe Acrobat Document (.pdf)"
+            };
+
+            //Act
+            var result = await _documentService.AddAsync(requestDto);
+
+            //Assert
+            Assert.True(result.IsSuccess, result.Errors.FirstOrDefault());
+            Assert.NotNull(result.Entity);
+            Assert.Empty(result.Errors);
+        }
+
+        [Fact]
+        public async Task ShouldGetAllDocumentsAsync()
+        {
+            //Act
+            var result = await _documentService.GetAllAsync();
+
+            //Assert
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public async Task ShouldUpdateDocumentAsync()
+        {
+            //Arrange
+            var id = 1;
+            var requestDto = new DocumentDto
+            {
+                Id = 1,
+                FileName = "Prueba",
+                OriginalName = "Prueba",
+                ContentType = "Adobe Acrobat Document (.pdf)"
+            };
+
+            //Act
+            var result = await _documentService.UpdateAsync(id, requestDto);
+
+            //Assert
+            Assert.True(result.IsSuccess, result.Errors.FirstOrDefault());
+            Assert.NotNull(result.Entity);
+            Assert.Equal(requestDto.FileName, result.Entity.FileName);
+        }
+
+        [Fact]
+        public async Task ShouldDeleteDocumentAsync()
+        {
+            //Arrange
+            var id = 1;
+
+            //Act
+            var result = await _documentService.DeleteByIdAsync(id);
+
+            //Assert
+            Assert.True(result.IsSuccess, result.Errors.FirstOrDefault());
+            Assert.NotNull(result.Entity);
+            Assert.True(result.Entity.Deleted);
+        }
+    }
+}
--- SolvexWorkShop.Services.Tests/UserServiceTests.cs
+++ SolvexWorkShop.Services.Tests/UserServiceTests.cs
@@ -1,13 +1,6 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using SolvexWorkshop.Model.Contexts;
-using SolvexWorkshop.Model.Entities;
-using SolvexWorkshop.Model.Repositories;
+﻿using SolvexWorkshop.Model.Repositories;
 using SolvexWorkShop.Bl.Dto;
-using SolvexWorkShop.Bl.Mapper;
 using SolvexWorkShop.Bl.Validations;
-using SolvexWorkShop.Core.Settings;
 using SolvexWorkShop.Services.Services;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,75 +8,20 @@ using Xunit;
 
 namespace SolvexWorkShop.Services.Tests
 {
-    public class UserServiceTests
+    public class UserServiceTests : IClassFixture<ServicesTestsFixture>
     {
         private readonly IUserService _userService;
-        private readonly User _emmanuel = new User
+        private readonly ServicesTestsFixture _fixture;
+
+        public UserServiceTests(ServicesTestsFixture fixture)
         {
-            Name = "Emmanuel",
-            MiddleName = "Enrique",
-            LastName = "Jimenez",
-            SecondLastName = "Pimentel",
-            Dob = new System.DateTime(1996, 06, 16),
-            DocumentType = Core.Enums.DocumentType.ID,
-            DocumentTypeValue = "22500851658",
-            Gender = Core.Enums.Gender.MALE,
-            UserName = "emmanuel",
-            Password = BCrypt.Net.BCrypt.HashPassword("Hola1234,")
-        };
+            _fixture = fixture;
 
-        private readonly User _maryelin = new User
-        {
-            Name = "Maryelin",
-            MiddleName = "V",
-            LastName = "Ramirez",
-            SecondLastName = "Alvarez",
-            Dob = new System.DateTime(2002, 11, 29),
-            DocumentType = Core.Enums.DocumentType.ID,
-            DocumentTypeValue = "40232338166",
-            Gender = Core.Enums.Gender.FEMALE,
-            UserName = "maryelinram",
-            Password = BCrypt.Net.BCrypt.HashPassword("Hola1234")
-        };
-
-        public UserServiceTests()
-        {
-            #region Autommaper
-
-            var mapper = new MapperConfiguration(x => x.AddProfile<MappingProfile>())
-               .CreateMapper();
-
-            #endregion
-
-            #region Repository
-
-            var optionsBuilder = new DbContextOptionsBuilder<WorkShopContext>();
-            optionsBuilder.UseInMemoryDatabase("WorkShop2");
-            var context = new WorkShopContext(optionsBuilder.Options);
-            context.AddRange(_emmanuel, _maryelin);
-            context.SaveChanges();
-
-            IUserRepository respository = new UserRepository(context);
-
-            #endregion
-
-            #region Validator
+            IUserRepository repository = new UserRepository(fixture.Context);
 
             var validator = new UserValidator();
 
-            #endregion
-
-            #region Option Settings
-
-            var settings = Options.Create(new JwtSettings
-            {
-                ExpiresInMinutes = 10,
-                Secret = "0263875b-b775-4426-938c-ab7c04c74b22"
-            });
-
-            #endregion
-
-            _userService = new UserService(respository, mapper, validator, settings);
+            _userService = new UserService(repository, fixture.Mapper, validator, fixture.Settings);
         }
 
         [Fact]
@@ -114,28 +52,34 @@ namespace SolvexWorkShop.Services.Tests
         }
 
         [Fact]
-        public async Task ShouldGetUserGeAllAsync()
+        public async Task ShouldGetUserToken()
+        {
+            //Arrange
+            var model = new AuthenticateRequestDto
+            {
+                UserName = "maryelinram",
+                Password = "marye1234"
+            };
+
+            //Act
+            var result = await _userService.GetToken(model);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<AuthenticateResponseDto>(result);
+            Assert.NotEmpty(result.Token);
+            Assert.Equal(_fixture._maryelin.UserName, result.UserName);
+
+        }
+
+        [Fact]
+        public async Task ShouldGetAllUsersAsync()
         {
             //Act
             var result = await _userService.GetAllAsync();
 
             //Assert
             Assert.NotEmpty(result);
-        }
-
-        [Fact]
-        public async Task ShouldDeleteUserAsync()
-        {
-            //Arrange
-            var id = 1;
-
-            //Act
-            var result = await _userService.DeleteByIdAsync(id);
-
-            //Assert
-            Assert.True(result.IsSuccess, result.Errors.FirstOrDefault());
-            Assert.NotNull(result.Entity);
-            Assert.True(result.Entity.Deleted);
         }
 
         [Fact]
@@ -159,10 +103,10 @@ namespace SolvexWorkShop.Services.Tests
                 Password = "Hola1234"
             };
 
+            //Act
             var result = await _userService.UpdateAsync(id, requestDto);
 
             //Assert
-
             Assert.True(result.IsSuccess, result.Errors.FirstOrDefault());
             Assert.NotNull(result.Entity);
             Assert.Equal(requestDto.UserName, result.Entity.UserName);
@@ -170,27 +114,18 @@ namespace SolvexWorkShop.Services.Tests
         }
 
         [Fact]
-        public async Task ShouldGetUserToken()
+        public async Task ShouldDeleteUserAsync()
         {
             //Arrange
-
-            var model = new AuthenticateRequestDto
-            {
-                UserName = "maryelinram",
-                Password = "Hola1234"
-            };
+            var id = 1;
 
             //Act
-
-            var result = await _userService.GetToken(model);
+            var result = await _userService.DeleteByIdAsync(id);
 
             //Assert
-
-            Assert.NotNull(result);
-            Assert.IsType<AuthenticateResponseDto>(result);
-            Assert.NotEmpty(result.Token);
-            Assert.Equal(_maryelin.UserName, result.UserName);
-
+            Assert.True(result.IsSuccess, result.Errors.FirstOrDefault());
+            Assert.NotNull(result.Entity);
+            Assert.True(result.Entity.Deleted);
         }
     }
 }
